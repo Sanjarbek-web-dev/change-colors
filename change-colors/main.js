a4ff3f61798ef063915729400225391c854915bc
Sanjarbek-web-dev <90724336+Sanjarbek-web-dev@users.noreply.github.com>
2021-11-17 07:18:02 +0300

    Add files via upload

---
+++ change-colors/main.js
@@ -0,0 +1,24 @@
+function myFunction() {
+    document.body.style.backgroundColor = "red";
+}
+function myFunction1() {
+    document.body.style.backgroundColor = "yellow";
+}
+function myFunction2() {
+    document.body.style.backgroundColor = "blue";
+}
+function myFunction3() {
+    document.body.style.backgroundColor = "green";
+}
+function myFunction4() {
+    document.body.style.backgroundColor = "goldenrod";
+}
+function myFunction5() {
+    document.body.style.backgroundColor = "white";
+}
+function myFunc() {
+    var element = document.body;
+    element.classList.toggle('dark-mode')
+}
+
+
